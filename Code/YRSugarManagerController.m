e3b91d93971d4066dad2a3b3a11b3abcf619a9c2
elliottcable <git@elliottcable.name>
2009-01-01 09:59:14 -0500

    Simplified things in the initializer

--- Code/YRSugarManagerController.m
+++ Code/YRSugarManagerController.m
@@ -13,22 +13,21 @@
   [super init];
   sugars = [NSMutableSet setWithCapacity:25];
   
-  YRSugarRepresentation *s1 = [YRSugarRepresentation sugarWithName:@"Sugar Manager"
+  [sugars addObject:[YRSugarRepresentation sugarWithName:@"Sugar Manager"
                                                             author:@"elliottcable"
                                                         identifier:@"name.elliottcable.Sugar.Manager"
                                                        downloadURL:@"http://github.com/elliottcable/sugar-manager.sugar/tarball/master"
-                                                           homeURL:@"http://github.com/elliottcable/sugar-manager.sugar"];
-  YRSugarRepresentation *s2 = [YRSugarRepresentation sugarWithName:@"Ruby"
+                                                           homeURL:@"http://github.com/elliottcable/sugar-manager.sugar"]];
+  [sugars addObject:[YRSugarRepresentation sugarWithName:@"Ruby"
                                                             author:@"elliottcable"
                                                         identifier:@"name.elliottcable.Sugar.Ruby"
                                                        downloadURL:@"http://github.com/elliottcable/ruby.sugar/tarball/master"
-                                                           homeURL:@"http://github.com/elliottcable/ruby.sugar"];
-  YRSugarRepresentation *s3 = [YRSugarRepresentation sugarWithName:@"Regex"
+                                                           homeURL:@"http://github.com/elliottcable/ruby.sugar"]];
+  [sugars addObject:[YRSugarRepresentation sugarWithName:@"Regex"
                                                             author:@"elliottcable"
                                                         identifier:@"name.elliottcable.Sugar.Regex"
                                                        downloadURL:@"http://github.com/elliottcable/regex.sugar/tarball/master"
-                                                           homeURL:@"http://github.com/elliottcable/regex.sugar"];
-  [sugars addObject:s1]; [sugars addObject:s2]; [sugars addObject:s3];
+                                                           homeURL:@"http://github.com/elliottcable/regex.sugar"]];
   
   return self;
 }
