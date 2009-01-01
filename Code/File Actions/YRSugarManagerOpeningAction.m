d6122947b79625d1ae15701f65ec936b8048870b
elliottcable <git@elliottcable.name>
2008-12-31 22:21:38 -0500

    Got a very basic start on the window opening bits

--- Code/File Actions/YRSugarManagerOpeningAction.m
+++ Code/File Actions/YRSugarManagerOpeningAction.m
@@ -4,7 +4,19 @@
 //
 
 #import "YRSugarManagerOpeningAction.h"
+#import <EspressoFileActions.h>
+#import <NSString+MRFoundation.h>
 
 @implementation YRSugarManagerOpeningAction
+
+- (BOOL)canPerformActionWithContext:(id)context {
+  return YES;
+}
+
+- (BOOL)performActionWithContext:(id)context error:(NSError **)outError {
+  [NSBundle loadNibNamed:@"SugarManager" owner:self];
+  
+  return YES;
+}
 
 @end
