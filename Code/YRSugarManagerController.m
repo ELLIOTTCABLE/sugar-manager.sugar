579ae7aca64fc6e35afab42bedef29564e94e360
elliottcable <git@elliottcable.name>
2009-01-02 01:21:38 -0500

    Added a hacky install proxy implementation

--- Code/YRSugarManagerController.m
+++ Code/YRSugarManagerController.m
@@ -31,6 +31,29 @@
 - (IBAction)installSugar:(id)sender {
   YRSugarRepresentation *sugar = [[sugarsController arrangedObjects] objectAtIndex:[sender clickedRow]];
   NSLog(@"- installSugar:%@", sugar);
+  
+  SEL selector = NSSelectorFromString([NSString stringWithFormat:@"installSugarFrom%@:", [[sugar downloadFormat] capitalizedString]]);
+  [self performSelector:selector withObject:sugar];
+}
+
+- (void)installSugarFromRaw:(id)sugar {
+  NSLog(@"- installSugarFromRaw:%@", sugar);
+}
+
+- (void)installSugarFromTgz:(id)sugar {
+  NSLog(@"- installSugarFromTgz:%@", sugar);
+}
+
+- (void)installSugarFromZip:(id)sugar {
+  NSLog(@"- installSugarFromZip:%@", sugar);
+}
+
+- (void)installSugarFromSvn:(id)sugar {
+  NSLog(@"- installSugarFromSvn:%@", sugar);
+}
+
+- (void)installSugarFromGit:(id)sugar {
+  NSLog(@"- installSugarFromGit:%@", sugar);
 }
 
 - (void)dealloc {
