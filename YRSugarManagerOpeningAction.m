733e9e3ce0184c160bd5705c69170216295b2704
elliottcable <git@elliottcable.name>
2009-01-05 19:35:41 -0500

    Added the YRSugarManagerOpeningAction class

---
+++ YRSugarManagerOpeningAction.m
@@ -0,0 +1,20 @@
+//
+//  YRSugarManagerOpeningAction.m
+//  SugarManager
+//
+
+#import "YRSugarManagerOpeningAction.h"
+#import <EspressoFileActions.h>
+#import <NSString+MRFoundation.h>
+
+@implementation YRSugarManagerOpeningAction
+
+- (BOOL)canPerformActionWithContext:(id)context {
+  return YES;
+}
+
+- (BOOL)performActionWithContext:(id)context error:(NSError **)outError {
+  return YES;
+}
+
+@end
